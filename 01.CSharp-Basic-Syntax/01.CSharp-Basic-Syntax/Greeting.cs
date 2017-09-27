8d20f31c658c1eceb28bc4e0829d82153af7776a
AndonMitev <mitevandon94@gmail.com>
2017-09-27 13:48:14 +0300

    first-push

---
+++ 01.CSharp-Basic-Syntax/01.CSharp-Basic-Syntax/Greeting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Startup
+{
+    static void Main()
+    {
+        var inputUserName = Console.ReadLine();
+        Console.WriteLine($"Hello, {inputUserName}!");
+    }
+}
+
