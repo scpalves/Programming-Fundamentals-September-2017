b9478db4da883c7a0aef807cd581d2762727d7a1
AndonMitev <mitevandon94@gmail.com>
2017-09-30 14:13:31 +0300

    Data-Types-And-Variables-Exercises

---
+++ TECH-ProgrammingFundamentals/07. DataTypesAndVariables-Exercises/05. BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BooleanVariable
+{
+    static void Main()
+    {
+        string inputBooleanVariable = Console.ReadLine();
+
+        if (inputBooleanVariable == "True")
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
+
---
+++ TECH-ProgrammingFundamentals/07. DataTypesAndVariables-Exercises/10. CenturiesToNanoseconds/CenturiesToNanoseconds.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CenturiesToNanoseconds
+{
+    static void Main()
+    {
+        byte centuries = byte.Parse(Console.ReadLine());
+        int years = centuries * 100;
+        int days = (int)(years * 365.2422);
+        int hours = days * 24;
+        long minutes = hours * 60;
+        long seconds = minutes * 60;
+        long milliSeconds = seconds * 1000;
+        decimal microSeconds = milliSeconds * 1000m;
+        decimal nanoSeconds = microSeconds * 1000m;
+
+        Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = " +
+            $"{minutes} minutes = {seconds} seconds = {milliSeconds} milliseconds = {microSeconds} microseconds = " +
+            $"{nanoSeconds} nanoseconds");
+
+    }
+}
+
